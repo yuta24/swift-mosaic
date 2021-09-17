e4d28f154697b0e168852da1a31961f34fd40da3
yuta24 <yuta24@bivre.com>
2021-09-17 20:56:13 +0900

    Rename Mosaic

--- Tests/MosaicTests/swift_mosaicTests.swift
+++ Tests/MosaicTests/swift_mosaicTests.swift
@@ -1,11 +1,10 @@
 import XCTest
 @testable import swift_mosaic
 
-final class swift_mosaicTests: XCTestCase {
+final class MosaicTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swift_mosaic().text, "Hello, World!")
     }
 }
