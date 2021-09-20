35044c796ce62d798e7c96f971c832e912a9f61b
yuta24 <yuta24@bivre.com>
2021-09-20 22:41:08 +0900

    Apply UInt/Float/Double/CGFloat to Sourceable

--- Sources/Mosaic/Mosaic.swift
+++ Sources/Mosaic/Mosaic.swift
@@ -11,6 +11,30 @@ extension Int: Sourceable {
     }
 }
 
+extension UInt: Sourceable {
+    public func toString() -> String {
+        return "\(self)"
+    }
+}
+
+extension Float: Sourceable {
+    public func toString() -> String {
+        return "\(self)"
+    }
+}
+
+extension Double: Sourceable {
+    public func toString() -> String {
+        return "\(self)"
+    }
+}
+
+extension CGFloat: Sourceable {
+    public func toString() -> String {
+        return "\(self)"
+    }
+}
+
 extension String: Sourceable {
     public func toString() -> String {
         return self
